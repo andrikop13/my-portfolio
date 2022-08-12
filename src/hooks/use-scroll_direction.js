328d2267e07163aa4da122e51636b71989013b50
Andreas Andrikopoulos <andreas.andrikopoulos1994@gmail.com>
2022-08-12 14:15:28 +0300

    Feat: add detect scroll-direction functionality and hide-show header respectively

---
+++ src/hooks/use-scroll_direction.js
@@ -0,0 +1,47 @@
+import { useEffect, useRef, useState } from "react";
+
+const THRESHOLD = 0;
+
+const useScrollDirection = () => {
+  const [scrollDirection, setScrollDirection] = useState("up");
+  const [scrolledToTop, setScrolledToTop] = useState(true);
+
+  const blocking = useRef(false);
+  const prevScrollY = useRef(0);
+
+  useEffect(() => {
+    prevScrollY.current = window.pageYOffset;
+
+    const updateScrollDirection = () => {
+      const scrollY = window.pageYOffset;
+
+      if (Math.abs(scrollY - prevScrollY.current) >= THRESHOLD) {
+        const newScrollDirection =
+          scrollY > prevScrollY.current ? "down" : "up";
+
+        setScrollDirection(newScrollDirection);
+
+        prevScrollY.current = scrollY > 0 ? scrollY : 0;
+      }
+
+      blocking.current = false;
+    };
+
+    const onScroll = () => {
+      setScrolledToTop(window.pageYOffset < 50);
+
+      if (!blocking.current) {
+        blocking.current = true;
+        window.requestAnimationFrame(updateScrollDirection);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [scrollDirection]);
+
+  return { scrollDirection, scrolledToTop };
+};
+
+export { useScrollDirection };
