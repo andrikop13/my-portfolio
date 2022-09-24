687460c48703198b4127937736c9e07eaa7c3015
andrikop13 <andreas.andrikopoulos1994@gmail.com>
2022-09-24 15:45:31 +0300

    Add reveal wrapper component on myImage

--- src/components/sections/Hero/MyImage.js
+++ src/components/sections/Hero/MyImage.js
@@ -11,24 +11,22 @@ const ImageContainer = styled.div`
 
   & .img-wrapper {
     position: relative;
-    transition: all 0.5s;
 
     & .hero-img {
-      animation: leftRotation 1.2s ;
-      -webkit-animation: leftRotation 1.2s;
+      animation: leftRotation 1s ;
+      -webkit-animation: leftRotation 1s;
       transform: rotate(-10deg);
       width: 29rem;
       position: relative;
       border-radius: var(--border-radius);
-      transition: var(--transition);
       mix-blend-mode: multiply;
       filter: grayscale(100%) contrast(1);
-      transition: all 0.5s 
+      transition: all 0.25s;
     }
 
     &::before {
-      animation: leftRotation 1.2s ;
-      -webkit-animation: leftRotation 1.2s ;
+      animation: leftRotation 1s ;
+      -webkit-animation: leftRotation 1s ;
       transform: rotate(-10deg);
       top: 0px;
       left: 0px;
@@ -39,8 +37,8 @@ const ImageContainer = styled.div`
     }
 
     &::after {
-      animation: rightRotation 1.2s ;
-      -webkit-animation: rightRotation 1.2s;
+      animation: rightRotation 1s ;
+      -webkit-animation: rightRotation 1s;
       transform: rotate(10deg);
       border: 2px solid var(--green);
       top: -3.25%;
@@ -56,7 +54,7 @@ const ImageContainer = styled.div`
       display: block;
       position: absolute;
       border-radius: var(--border-radius);
-      transition: var(--transition);
+      transition: all 0.25s;
     }
 
     &:hover,
@@ -121,25 +119,23 @@ const MyImage = () => {
   useEffect(() => {
     setTimeout(() => {
       setStageTime(true);
-    }, 100);
+    }, 250);
   });
 
   const transProps = {
     in: stageTime,
     classNames: "fadedown",
-    timeout: 0,
+    timeout: 200,
   };
 
   return (
-    stageTime && (
-      <CSSTransition nodeRef={ref} {...transProps}>
-        <ImageContainer>
-          <div className="img-wrapper">
-            <img src={image} className="hero-img" alt="My Profile"></img>
-          </div>
-        </ImageContainer>
-      </CSSTransition>
-    )
+    <CSSTransition nodeRef={ref} {...transProps}>
+      <ImageContainer>
+        <div className="img-wrapper">
+          <img src={image} className="hero-img" alt="My Profile"></img>
+        </div>
+      </ImageContainer>
+    </CSSTransition>
   );
 };
 
