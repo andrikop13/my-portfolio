4a14e57dd89ed898e87ea82fc84d145224dd22ed
andrikop13 <a.andrikopoulos@meazon.com>
2024-08-05 13:13:04 +0300

    compress new image with webp

--- src/components/sections/Contact/Map/Marker.js
+++ src/components/sections/Contact/Map/Marker.js
@@ -1,28 +1,27 @@
 import { useEffect, useRef } from "react";
 import { Marker } from "react-leaflet";
 import L from "leaflet";
-import { MarkerIcon } from "@components/sections";
 
 function MarkerWithOpenPopup(props) {
-  const LeafIcon = L.Icon.extend({
-    options: {},
-  });
+    const LeafIcon = L.Icon.extend({
+        options: {},
+    });
 
-  const customIcon = new LeafIcon({
-    iconUrl: MarkerIcon,
-    iconSize: [30, 60],
-    iconAnchor: [10, 58], // point of the icon which will correspond to marker's location
-    popupAnchor: [5, -57],
-  });
+    const customIcon = new LeafIcon({
+        iconUrl: require("./marker.svg"),
+        iconSize: [30, 60],
+        iconAnchor: [10, 58], // point of the icon which will correspond to marker's location
+        popupAnchor: [5, -57],
+    });
 
-  const markerRef = useRef();
+    const markerRef = useRef();
 
-  useEffect(() => {
-    if (!markerRef.current) return;
-    markerRef.current.openPopup();
-  }, []);
+    useEffect(() => {
+        if (!markerRef.current) return;
+        markerRef.current.openPopup();
+    }, []);
 
-  return <Marker ref={markerRef} {...props} icon={customIcon} />;
+    return <Marker ref={markerRef} {...props} icon={customIcon} />;
 }
 
 export default MarkerWithOpenPopup;
